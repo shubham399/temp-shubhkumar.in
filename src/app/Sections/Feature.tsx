6a5dc5aa8c1b0922ab6ceee8f9f96743d8cc23c5
Shubham Kumar <hello@shubhkumar.in>
2023-07-08 16:03:18 +0530

    Animate when in View

--- src/app/Sections/Feature.tsx
+++ src/app/Sections/Feature.tsx
@@ -1,6 +1,39 @@
+"use client"
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 export default function Feature() {
-    return (<section className="min-w-screen min-h-screen snap-start	flex flex-col  items-center justify-center">
-        <h2 className="text-zinc-500">TEST</h2>
+    const ref = useRef<HTMLElement>(null);
+    const [isIntersecting, setIntersecting] = useState(false);
+
+    useEffect(() => {
+        if (!ref.current) return;
+        const observer = new IntersectionObserver(([entry]) =>
+            setIntersecting(entry.isIntersecting),
+        );
+
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, []);
+
+    return (<section ref={ref} className="min-w-screen min-h-screen snap-start	flex flex-col  items-center justify-center">
+        <div className={`my-16 text-center ${isIntersecting ? "animate-fade-in" : "hidden"}`}
+
+        >
+            <h2 className="text-md text-zinc-500 ">
+                Hi, my name is Shubham Kumar, I &#39;  m building software
+                solutions at{" "}
+                <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://airfi.aero"
+                    className="underline duration-500 hover:text-zinc-300"
+                >
+                    airfi.aero
+                </Link>
+
+                <br />
+            </h2>
+        </div>
     </section>)
 }
--- src/app/Sections/Hero.tsx
+++ src/app/Sections/Hero.tsx
@@ -1,30 +1,27 @@
-import Link from "next/link";
+"use client"
+import { useEffect, useRef, useState } from "react";
 
 export default function Hero() {
-    return (
-        <section className='min-h-screen flex flex-col items-center justify-center'>
+    const ref = useRef<HTMLElement>(null);
+    const [isIntersecting, setIntersecting] = useState(false);
 
-            <div className="hidden h-px w-screen animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-            <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+    useEffect(() => {
+        if (!ref.current) return;
+        const observer = new IntersectionObserver(([entry]) =>
+            setIntersecting(entry.isIntersecting),
+        );
+
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, []);
+    return (
+        <section ref={ref} className='min-h-screen flex flex-col items-center justify-center'>
+
+            <div className={`hidden h-px w-screen bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 ${isIntersecting && "animate-glow md:block animate-fade-left"}`} />
+            <h1 className={`z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ${isIntersecting && "animate-title"}`}>
                 Shubham Kumar
             </h1>
 
-            <div className="hidden h-px w-screen animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-            <div className="my-16 text-center animate-fade-in">
-                <h2 className="text-sm text-zinc-500 ">
-                    Hi, my name is Shubham Kumar, I &#39;  m building software
-                    solutions at{" "}
-                    <Link
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://airfi.aero"
-                        className="underline duration-500 hover:text-zinc-300"
-                    >
-                        airfi.aero
-                    </Link>
-
-                    <br />
-                </h2>
-            </div>
+            <div className={`hidden h-px w-screen md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 ${isIntersecting && "animate-glow md:block animate-fade-right"}`} />
         </section>)
 }
