bdd87e75c9b9117e361d28ba001ea8a998a23b3f
Shubham Kumar <hello@shubhkumar.in>
2023-07-09 11:07:56 +0530

    Clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Hero from './Sections/Hero';
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-y-auto overflow-x- bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-y-auto overflow-x-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
         <Navigation isHome={true} />
         <ScrollUp />
         <Hero />
