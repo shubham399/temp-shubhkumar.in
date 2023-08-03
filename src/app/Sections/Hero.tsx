b10ba294e29cac35c70e38ee48570ebbdffeff87
Shubham Kumar <hello@shubhkumar.in>
2023-08-03 20:53:56 +0530

    Added default opacity to be 0

--- src/app/Sections/Hero.tsx
+++ src/app/Sections/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
         <section className='min-h-screen flex flex-col items-center justify-center'>
 
             <div className={`hidden h-px w-screen bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 ${isIntersecting && "animate-glow md:block animate-fade-left"}`} />
-            <h1 ref={ref} className={`z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ${isIntersecting && "animate-title"}`}>
+            <h1 ref={ref} className={`z-10 opacity-0 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ${isIntersecting && "animate-title"}`}>
                 Shubham Kumar
             </h1>
 
